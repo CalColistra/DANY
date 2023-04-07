3a1ff841989698e01bd6ef0d9f88e8e76a77372e
Cal Colistra <calcolistra10@gmail.com>
2023-04-07 17:35:03 -0400

    attempt1

--- Visual Evidence Request/timeRequestRepeatingSection.js
+++ Visual Evidence Request/timeRequestRepeatingSection.js
@@ -27,13 +27,34 @@ function calcDiff() {
     var dvrStartHr = row.find(".dvrStartHr input").val();
     var dvrStartMin = row.find(".dvrStartMin input").val();
     var dvrStartSec = row.find(".dvrStartSec input").val();
+    dvrStartHr = Number(dvrStartHr);
+    dvrStartMin = Number(dvrStartMin);
+    dvrStartSec = Number(dvrStartSec);
     
     var dvrEndHr = row.find(".dvrEndHr input").val();
     var dvrEndMin = row.find(".dvrEndMin input").val();
     var dvrEndSec = row.find(".dvrEndSec input").val();
+    dvrEndHr = Number(dvrEndHr);
+    dvrEndMin = Number(dvrEndMin);
+    dvrEndSec = Number(dvrEndSec);
     
-    testString = "DVR Start: "+dvrStart+" DVR End: "+ dvrEnd+",";
+    if ( (isNaN(dvrStartHr)) || (isNaN(dvrStartMin)) ||(isNaN(dvrStartSec)) ||
+       (isNaN(dvrEndHr)) ||(isNaN(dvrEndMin)) ||(isNaN(dvrEndSec)) ) {
+      alert("You must only enter numbers in DVR Times.");
+      NWF$('#'+totalDuration).val("You must only enter numbers in DVR Times.");
+    }
+    else {
+      hrDif = dvrEndHr - dvrStartHr;
+      minDif = dvrEndMin - dvrStartMin;
+      secDif = dvrEndSec - dvrStartSec;
+      //convert min to hour:
+      minDif = minDif/60;
+      //convert sec to hour:
+      secDif = secDiff/3600;
+      // calc difference in hours:
+      difference = hrDif + minDif + secDif;
+      NWF$('#'+totalDuration).val(difference)
+    }
+    
   });
-  testString = testString.toString();
-  NWF$('#'+totalDuration).val(testString)
 }
