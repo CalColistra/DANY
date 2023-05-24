e366c1c48e9f894316749f06114afbac85afc745
Cal Colistra <calcolistra10@gmail.com>
2023-05-24 12:19:34 -0400

    fixed

--- Visual Evidence Request/GrandJuryScheduling/schedulerForm.js
+++ Visual Evidence Request/GrandJuryScheduling/schedulerForm.js
@@ -1,100 +1,65 @@
+var callCounter = 1;
 NWF$().ready(function () {
-    NWF$('.ctl_clc_date1SelectionsAm').change(function() {
+    NWF$('.ctl_clc_date1Selections').change(function() {
         var timeSlot1 = NWF$("#"+ctl_clc_date1TimeSlot);
         timeSlot1 = timeSlot1.val();
-        var selectionsAm = NWF$("#"+ctl_clc_date1SelectionsAm);
-        selectionsAm = selectionsAm.val();
-        var selectionsPm = NWF$("#"+ctl_clc_date1SelectionsPm);
-        selectionsPm = selectionsPm.val();
-        checkSelection(timeSlot1, 1, selectionsAm, selectionsPm);
+        var selections = NWF$("#"+ctl_clc_date1Selections);
+        selections = selections.val();
+        checkSelection(timeSlot1, 1, selections);
     });
-    NWF$('.ctl_clc_date1SelectionsPm').change(function() {
-        var timeSlot1 = NWF$("#"+ctl_clc_date1TimeSlot);
-        timeSlot1 = timeSlot1.val();
-        var selectionsAm = NWF$("#"+ctl_clc_date1SelectionsAm);
-        selectionsAm = selectionsAm.val();
-        var selectionsPm = NWF$("#"+ctl_clc_date1SelectionsPm);
-        selectionsPm = selectionsPm.val();
-        checkSelection(timeSlot1, 1, selectionsAm, selectionsPm);
-    });
-    NWF$('.ctl_clc_date2SelectionsAm').change(function() {
+
+    NWF$('.ctl_clc_date2Selections').change(function() {
         var timeSlot2 = NWF$("#"+ctl_clc_date2TimeSlot);
         timeSlot2 = timeSlot2.val();
-        var selectionsAm = NWF$("#"+ctl_clc_date2SelectionsAm);
-        selectionsAm = selectionsAm.val();
-        var selectionsPm = NWF$("#"+ctl_clc_date2SelectionsPm);
-        selectionsPm = selectionsPm.val();
-        checkSelection(timeSlot2, 2, selectionsAm, selectionsPm);
+        var selections = NWF$("#"+ctl_clc_date2Selections);
+        selections = selections.val();
+        checkSelection(timeSlot2, 2, selections);
     });
-    NWF$('.ctl_clc_date2SelectionsPm').change(function() {
-        var timeSlot2 = NWF$("#"+ctl_clc_date2TimeSlot);
-        timeSlot2 = timeSlot2.val();
-        var selectionsAm = NWF$("#"+ctl_clc_date2SelectionsAm);
-        selectionsAm = selectionsAm.val();
-        var selectionsPm = NWF$("#"+ctl_clc_date2SelectionsPm);
-        selectionsPm = selectionsPm.val();
-        checkSelection(timeSlot2, 2, selectionsAm, selectionsPm);
-    });
-    NWF$('.ctl_clc_date3SelectionsAm').change(function() {
+    NWF$('.ctl_clc_date3Selections').change(function() {
         var timeSlot3 = NWF$("#"+ctl_clc_date3TimeSlot);
         timeSlot3 = timeSlot3.val();
-        var selectionsAm = NWF$("#"+ctl_clc_date3SelectionsAm);
-        selectionsAm = selectionsAm.val();
-        var selectionsPm = NWF$("#"+ctl_clc_date3SelectionsPm);
-        selectionsPm = selectionsPm.val();
-        checkSelection(timeSlot3, 3, selectionsAm, selectionsPm);
+        var selections = NWF$("#"+ctl_clc_date3Selections);
+        selections = selections.val();
+        checkSelection(timeSlot3, 3, selections);
     });
-    NWF$('.ctl_clc_date3SelectionsPm').change(function() {
-        var timeSlot3 = NWF$("#"+ctl_clc_date3TimeSlot);
-        timeSlot3 = timeSlot3.val();
-        var selectionsAm = NWF$("#"+ctl_clc_date3SelectionsAm);
-        selectionsAm = selectionsAm.val();
-        var selectionsPm = NWF$("#"+ctl_clc_date3SelectionsPm);
-        selectionsPm = selectionsPm.val();
-        checkSelection(timeSlot3, 3, selectionsAm, selectionsPm);
-    });
-    NWF$('.ctl_clc_date4SelectionsAm').change(function() {
+
+    NWF$('.ctl_clc_date4Selections').change(function() {
         var timeSlot4 = NWF$("#"+ctl_clc_date4TimeSlot);
         timeSlot4 = timeSlot4.val();
-        var selectionsAm = NWF$("#"+ctl_clc_date4SelectionsAm);
-        selectionsAm = selectionsAm.val();
-        var selectionsPm = NWF$("#"+ctl_clc_date4SelectionsPm);
-        selectionsPm = selectionsPm.val();
-        checkSelection(timeSlot4, 4, selectionsAm, selectionsPm);
+        var selections = NWF$("#"+ctl_clc_date4Selections);
+        selections = selections.val();
+        checkSelection(timeSlot4, 4, selections);
     });
 });
-async function checkSelection(timeSlot, date, selectionsAm, selectionsPm) {
-    var selectionCount = 0;
-    console.log("Am selections: "+selectionsAm);
-    console.log("TimeSlot: "+timeSlot);
-    for (let i = 0; i < selectionsAm.length; i++) {
-        var currentChar = selectionsAm.charAt(i);
-        if (currentChar == ";") selectionCount = selectionCount+1;
+async function checkSelection(timeSlot, date, selections) {
+    if (callCounter < 5) {
+        callCounter = callCounter +1;
+        return;
     }
-    for (let i = 0; i < selectionsPm.length; i++) {
-        var currentChar = selectionsPm.charAt(i);
-        if (currentChar == ";") selectionCount = selectionCount+1;
+    else {
+        var selectionCount = 0;
+        for (let i = 0; i < selections.length; i++) {
+            var currentChar = selections.charAt(i);
+            if (currentChar == ";") selectionCount = selectionCount+1;
+        }
+        var timeSelected = selectionCount*15;
+        var timeSlotFilledResult = timeSelected - timeSlot;
+        if (date == 1) {
+            var txtBoxFilled = NWF$('#'+ctl_txt_timeSlot1Filled);
+            txtBoxFilled.val(timeSlotFilledResult).trigger("blur");
+        }
+        else if (date == 2) {
+            var txtBoxFilled = NWF$('#'+ctl_txt_timeSlot2Filled);
+            txtBoxFilled.val(timeSlotFilledResult).trigger("blur");
+        }
+        else if (date == 3) {
+            var txtBoxFilled = NWF$('#'+ctl_txt_timeSlot3Filled);
+            txtBoxFilled.val(timeSlotFilledResult).trigger("blur");
+        }
+        else if (date == 4) {
+            var txtBoxFilled = NWF$('#'+ctl_txt_timeSlot4Filled);
+            txtBoxFilled.val(timeSlotFilledResult).trigger("blur");
+        }
     }
-    var timeSelected = selectionCount*15;
-    console.log("Selected Time: "+timeSelected);
-    var timeSlotFilledResult;
-    if (timeSelected == timeSlot) timeSlotFilledResult = "exact";
-    else if (timeSelected > timeSlot) timeSlotFilledResult = "over";
-    else if (timeSelected < timeSlot) timeSlotFilledResult = "under";
-    if (date == 1) {
-        var txtBoxFilled = NWF$('#'+ctl_txt_timeSlot1Filled);
-        txtBoxFilled.val(timeSlotFilledResult);
-    }
-    else if (date == 2) {
-        var txtBoxFilled = NWF$('#'+ctl_txt_timeSlot2Filled);
-        txtBoxFilled.val(timeSlotFilledResult);
-    }
-    else if (date == 3) {
-        var txtBoxFilled = NWF$('#'+ctl_txt_timeSlot3Filled);
-        txtBoxFilled.val(timeSlotFilledResult);
-    }
-    else if (date == 4) {
-        var txtBoxFilled = NWF$('#'+ctl_txt_timeSlot4Filled);
-        txtBoxFilled.val(timeSlotFilledResult).trigger("blur");
-    }
+    
 }
